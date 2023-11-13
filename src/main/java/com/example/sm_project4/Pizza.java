9e8ef2d30023d2c51206af223fff82b6c0833893
SCooldude <sammanpandey7@gmail.com>
2023-11-13 09:42:54 -0500

    not finished setting the connection

--- src/main/java/com/example/sm_project4/Pizza.java
+++ src/main/java/com/example/sm_project4/Pizza.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 
 public abstract class Pizza {
     protected ArrayList <Topping> toppings; //Topping is an enum class
-    protected Size size; //Size is an enum class
-    protected Sauce sauce; //Sauce is an enum class
+    protected Size size; //enum
+    protected Sauce sauce;  //enum
     protected boolean extraSauce;
     protected boolean extraCheese;
 
